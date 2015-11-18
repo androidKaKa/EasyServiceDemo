c375897a683c640900d596273b220963b7c935e3
DIM <dimemail@163.com>
2015-11-18 08:13:04 +0800

    commit a service demo

---
+++ gen/phlin/demo/main/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package phlin.demo.main;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/phlin/demo/service/BaseService.java
@@ -0,0 +1,13 @@
+package phlin.demo.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BaseService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+}
